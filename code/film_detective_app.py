6e8d405af2108f68f8845beb3a10fdc6cd98e439
nolan <nolan@smurro.com>
2023-12-05 11:08:53 -0800

    streamlit and file tree updates

--- code/film_detective_app.py
+++ code/film_detective_app.py
@@ -4,8 +4,8 @@ from PIL import Image
 import numpy as np
 # import matplotlib.cm as cm
 import tensorflow as tf
-# from image_preprocessing import crop_resize
-import os
+from image_preprocessing import crop_resize
+# import os
 
 # def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
 #     grad_model = tf.keras.models.Model(
@@ -29,34 +29,34 @@ import os
 #     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
 #     return heatmap.numpy()
 
-# def preprocess_image(image):
-#     processed_image = crop_resize(image, (700,700))
-#     processed_image = np.array(processed_image)
-#     processed_image = processed_image / 255.0
-#     processed_image = np.expand_dims(processed_image, axis=0)
-#     return processed_image
+def preprocess_image(image):
+    processed_image = crop_resize(image, (700,700))
+    processed_image = np.array(processed_image)
+    processed_image = processed_image / 255.0
+    processed_image = np.expand_dims(processed_image, axis=0)
+    return processed_image
     
-# model = load_model('../model_checkpoints/checkpoint_20-0.81.h5')
+model = load_model('model_checkpoints/checkpoint_20-0.81.h5')
 # last_conv_layer_name = 'conv2d_4'
 
 st.title('Film Detective')
 
-st.write(os.getcwd())
+# st.write(os.getcwd())
 
-# uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
 
-# if uploaded_file is not None:
-#     image = Image.open(uploaded_file)
-#     st.image(image, caption='Uploaded Image', use_column_width=True)
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
     
-#     if st.button('Classify Image'):
-#         processed_image = preprocess_image(image)
+    if st.button('Classify Image'):
+        processed_image = preprocess_image(image)
     
-#         predictions = model.predict(processed_image)
+        predictions = model.predict(processed_image)
     
-#         predicted_class = 'Film' if predictions[0][0] > 0.5 else 'Digital'
+        predicted_class = 'Film' if predictions[0][0] > 0.5 else 'Digital'
     
-#         st.write(f'Predicted Class: {predicted_class}')
+        st.write(f'Predicted Class: {predicted_class}')
         
 #         heatmap = make_gradcam_heatmap(processed_image, model, last_conv_layer_name)
         
